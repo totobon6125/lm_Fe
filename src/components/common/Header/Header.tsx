9fac1c7979c0dd02c0e4fc902fb1a6cc8ab78bc2
totobon <totobon6125@gmail.com>
2023-11-03 02:43:00 +0900

    헤더 변경

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -7,7 +7,10 @@ import { useRecoilState } from "recoil";
 import { isLoginState } from "../../../recoil/atoms/UserState";
 import { useEffect } from "react";
 import { useLanguage } from "../../../util/Locales/useLanguage";
-import textlogo from "../../../asset/localMingleImages/textlogo.png";
+import textlogo from "../../../asset/localMingleImages/textlogo.png"; 
+import kologo from "../../../asset/languageImages/kologo.png";
+import enlogo from "../../../asset/languageImages/enlogo.png";
+import jplogo from "../../../asset/languageImages/jplogo.png";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
@@ -51,7 +54,11 @@ const Header: React.FC = () => {
       {isLogin === false ? (
         /* 로그인 전 */
         <St.HeaderWrap>
-          <img onClick={goToMain} src={textlogo} alt="로컬밍글로고" />
+          <img
+            onClick={goToMain}
+            src={textlogo}
+            alt="로컬밍글로고"
+          />
           <St.HeaderBtns>
             <button onClick={login}>{t("로그인")}</button>
             {/* 다국어 지원 */}
@@ -88,7 +95,11 @@ const Header: React.FC = () => {
       ) : (
         /* 로그인 후 */
         <St.HeaderWrap>
-          <img onClick={goToMain} src={textlogo} alt="로컬밍글로고" />
+          <img
+            onClick={goToMain}
+            src={textlogo}
+            alt="로컬밍글로고"
+          />
           <St.HeaderBtns>
             <button onClick={goToMyPage}>{t("마이페이지")}</button>
             <button onClick={logout}>{t("로그아웃")}</button>
